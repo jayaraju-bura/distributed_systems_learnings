8d419a60110f1d882be8a8fd76929d8da78a7d82
Jayaraju bura <92459601+jayaraju-bura@users.noreply.github.com>
2023-01-15 17:13:27 +0530

    Update wordcounter.go

--- mapreduce/src/mrapps/wordcounter.go
+++ mapreduce/src/mrapps/wordcounter.go
@@ -1,1 +1,28 @@
+package main
 
+import (
+        "https://github.com/jayaraju-bura/distributed_systems_learnings/tree/main/mapreduce/src/mr"
+)
+import (
+        "unicode"
+        "strings"
+        "stringconv"
+)
+
+func Map(filename string, contents string) []mr.KeyValue {
+  funtn := func(r rune) bool { !unicode.IsLetter(r)}
+  
+  kva := []mr.KeyValue{}
+  words := strings.FieldFunc(contents, funtn)
+  for _, wrd := range words {
+    kv := mr.KeyValue{wrd, "1"}
+    kva = append(kva, kv)
+  }
+  
+  return kva
+}
+
+func Reduce(key string, values []string) string {
+  
+  return stringconv.Itoa(len(values))
+}
