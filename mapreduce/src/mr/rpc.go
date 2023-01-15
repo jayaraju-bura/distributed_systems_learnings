d9fea257b8e3f31a88e38c6eb33f5296fb034aaa
Jayaraju bura <92459601+jayaraju-bura@users.noreply.github.com>
2023-01-15 18:54:30 +0530

    Update rpc.go

--- mapreduce/src/mr/rpc.go
+++ mapreduce/src/mr/rpc.go
@@ -1,1 +1,48 @@
+package mr
 
+
+import "os"
+import "strconv"
+
+
+type ExampleArgs struct {
+        X int
+}
+
+type ExampleReply {
+        Y int
+}
+
+type MrArgs struct {
+
+}
+
+type MrReply struct  {
+        MapFileName string
+        TaskType    string
+        Index       int
+        NReduce     int
+        Files       []string
+}
+type NotifyReply {
+
+}
+
+type NotifyIntermediateArgs {
+        Reduceindex int
+        File        string
+}
+
+type NotifyMapSuccessArgs {
+        File string
+}
+
+type NotifyReduceSuccessArgs {
+        Reduceindex int
+}
+
+func mastersock() string {
+  sock := "/var/tmp/456-mr"
+  sock += strconv.Itoa(os.Getuid())
+  return sock
+}
