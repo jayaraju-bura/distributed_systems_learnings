051300fc84a0732222e3f9025b8228524c149c97
Jayaraju bura <92459601+jayaraju-bura@users.noreply.github.com>
2023-01-15 16:59:55 +0530

    Update mrmaster.go
    create master caller

--- mapreduce/src/main/mrmaster.go
+++ mapreduce/src/main/mrmaster.go
@@ -1,1 +1,27 @@
+package main
 
+
+import (
+        "https://github.com/jayaraju-bura/distributed_systems_learnings/tree/main/mapreduce/src/mr"
+)
+
+import (
+        "time"
+        "os"
+        "fmt"
+)
+
+func main() {
+  
+  if len(os.Args) < 2 {
+           fmt.Fprintf(os.Stderr, "Usage : go run mrmaster.go <input_file>")
+           os.Exit(1)
+  }
+  
+  master_inst := mr.MakeMaster(os.Args[1:], 10)
+  for master_inst.Done() == false {
+                    time.Sleep(time.Second)
+  }
+  
+  time.Sleeo(time.Second)
+}
