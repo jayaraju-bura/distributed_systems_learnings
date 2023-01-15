59dc7f173a2c7b92bc733e2a02de9afc3dabd144
Jayaraju bura <92459601+jayaraju-bura@users.noreply.github.com>
2023-01-15 17:37:02 +0530

    Update mrworker.go

--- mapreduce/src/main/mrworker.go
+++ mapreduce/src/main/mrworker.go
@@ -1,1 +1,44 @@
+package main
 
+import (
+         "https://github.com/jayaraju-bura/distributed_systems_learnings/tree/main/mapreduce/src/mr"
+)
+import (
+        "os"
+        "fmt"
+        "log"
+        "plugin"
+)
+
+func main() {
+  if len(os.Args) != 2 {
+      fmt.Fprintf(os.Stderr, "Usage: mrworker <input.so>")
+      os.Exit(1)
+  }
+  
+  mapf, reducef := loadPlugin(os.Args[1])
+  mr.Worker(mapf, reducef)
+}
+
+func loadPlugin(filename string) (func(string, string) []mr.KeyValue, func(string, []string) string) {
+  
+  plg, err := plugin.Open(filename)
+  if err != nil {
+    log.Fatalf("Cannot load plugin %v", filename)
+  }
+  
+  xmapf, err := plg.Lookup("Map")
+  if err != nil {
+    log.Fatalf("Cannot find Map in %v", filename)
+  )
+   mapf := xmapf.(func(string, string) []mr.KeyValue)
+    
+   xreducef, err := plg.Lookup("Reduce")
+    if err != nil {
+      log.Fatalf("Cannot find Reduce in %v", filename)
+    }
+    
+    reducef := xreducef.(func(string, []string) string)
+    return mapf, reducef
+    
+}
